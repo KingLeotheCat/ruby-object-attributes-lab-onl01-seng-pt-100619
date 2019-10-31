f9e8908bfe200b4aca499ee8f93e4f1b445e1baa
Lenny Lee <lennylee91@gmail.com>
2019-10-31 22:41:11 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,23 +1,24 @@
 class Person
   
-  attr_accessor :name
+  attr_accessor :name, :job
+  
 end
   
-  def name=(name)
-    @name = name
-  end
+#   def name=(name)
+#     @name = name
+#   end
   
-  def name
-    @name
-  end
+#   def name
+#     @name
+#   end
   
-  def job=(job)
-    @job = job
-  end
+#   def job=(job)
+#     @job = job
+#   end
   
-  def job
-    @job
+#   def job
+#     @job
     
-  end
-end
+#   end
+# end
 
